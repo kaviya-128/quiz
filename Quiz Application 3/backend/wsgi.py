8e6e141893434f3fb71a05e8df1436590c42a8a9
Kaviya <kavinat08032005@gmail.com>
2026-03-13 00:03:45 +0530

    Update wsgi.py

--- Quiz Application 3/backend/wsgi.py
+++ Quiz Application 3/backend/wsgi.py
@@ -1,15 +1,3 @@
-from importlib import import_module
+from app import create_app
 
-mod = import_module("app")   # imports app.py
-
-if hasattr(mod, "app"):
-    app = mod.app
-elif hasattr(mod, "application"):
-    app = mod.application
-elif hasattr(mod, "create_app"):
-    app = mod.create_app()
-else:
-    raise RuntimeError(
-        "No Flask app found. Define `app = Flask(__name__)` or `application = Flask(__name__)` "
-        "or a `create_app()` function inside app.py"
-    )
+app = create_app()
